5c44138454ac8cd4439059b215ba30ce1a03c1b3
yashvimaniar <yashvimaniar48@gmail.com>
2026-01-13 22:18:52 +0530

    Add files via upload

---
+++ t4_gst_tax_amt.py
@@ -0,0 +1,6 @@
+bill_amt=float(input("Enter bill amount:"))
+tax_rate=float(input("Enter tax rate:"))
+gst_amt=(bill_amt*tax_rate)/100
+print("GST Tax amount= ",gst_amt)
+# tot_bill_amt=bill_amt+gst_amt
+# print("Total bill amount= ",tot_bill_amt)
