6eef264feb6fb350ab0fa9c3ec89e299bd1c8218
yashvimaniar <yashvimaniar48@gmail.com>
2026-01-20 09:14:06 +0530

    Add files via upload

--- t23_day_in_month_if_elsif.py
+++ t23_day_in_month_if_elsif.py
@@ -1,8 +1,10 @@
 mon=int(input("Enter month number:"))
-
-if mon==1 or mon==3 or mon==5 or mon==7 or mon==8 or mon==10 or mon==12:
-    print("this month has 31 days")
-elif mon==2:
-        print("this month has 28 or 29 days")
+if mon<=0 or mon>12:
+    print("Invalid month")
 else:
-    print("this month has 30 days")
+    if mon==1 or mon==3 or mon==5 or mon==7 or mon==8 or mon==10 or mon==12:
+        print("this month has 31 days")
+    elif mon==2:
+        print("this month has 28 or 29 days")
+    else:
+        print("this month has 30 days")
--- t24_milleniumyr_if_elsif.py
+++ t24_milleniumyr_if_elsif.py
@@ -1,8 +1,9 @@
 y=int(input("Enter a year: "))
 
-if y%1000==0:
-    print(y,"is Millennium year")
-elif y%1000!=0:
-    print(y,"is not Millennium year")
+if y<=0:
+    print("Invalid year")
 else:
-    print("Invalid input")
+    if y%1000==0:
+        print(y,"is Millennium year")
+    else:
+        print(y,"is not Millennium year")
